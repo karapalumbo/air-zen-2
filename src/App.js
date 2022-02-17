74088cb009650b14de680bb2114af1d5c4deb286
Kara <palumbokara@yahoo.com>
2022-02-16 17:35:32 -0700

    added logo

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import "./App.css";
 import ListOfFlight from "./ListOfFlights";
+import AirzenLogo from "./images/airzenLogo.png";
 
 function App() {
   return (
     <div className="App">
-      <header className="AirZen2"></header>
+      <header className="airzen2"></header>
+      <img className="logo" src={AirzenLogo}></img>
       <ListOfFlight />
     </div>
   );
